3ab4d8c58152012ad71f8862b422f7d2340e4186
DeveloperYS-web <jjul12345j@gmail.com>
2026-07-03 17:52:38 +0300

    5.18. Больше подробностей (часть 1)

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,6 +1,52 @@
-function Map(): JSX.Element {
+import { useEffect, useRef } from 'react';
+import leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import { OfferPreview } from '@/types/offer';
+
+type MapProps = {
+  offers: OfferPreview[];
+};
+
+function Map({ offers }: MapProps): JSX.Element {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+  const mapInstanceRef = useRef<leaflet.Map | null>(null);
+
+  useEffect(() => {
+    if (mapRef.current !== null && mapInstanceRef.current === null) {
+      const map = leaflet.map(mapRef.current);
+
+      map.setView(
+        [
+          offers[0].city.location.latitude,
+          offers[0].city.location.longitude,
+        ],
+        offers[0].city.location.zoom
+      );
+
+      leaflet
+        .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+        .addTo(map);
+
+      mapInstanceRef.current = map;
+
+      offers.forEach((offer) => {
+        leaflet
+          .marker([
+            offer.location.latitude,
+            offer.location.longitude,
+          ])
+          .addTo(map);
+      });
+    }
+  }, [offers]);
+
   return (
-    <section className="cities__map map"></section>
+    <section
+      className="cities__map map"
+      ref={mapRef}
+    >
+    </section>
   );
 }
 
